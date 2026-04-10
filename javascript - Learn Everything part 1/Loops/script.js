fbb86d2213613263edb0fca71cce9992da0aaa11
paawan-web-ai <matrixescape345@gmail.com>
2026-04-10 23:18:06 +0530

    loops

---
+++ javascript - Learn Everything part 1/Loops/script.js
@@ -0,0 +1,99 @@
+// repeat karne ko loop kahete hai
+// for while do-while forin forof foreach
+
+// for (let i = 1; i < 30; i++) {
+//   console.log(i);
+//   if (i === 12) break;
+// }
+
+// ------------------------------
+
+// for (let i = 1; i < 30; i++) {
+//   if (i === 28) continue;
+//   console.log(i);
+// }
+
+// practice
+
+// print numbers from 1 to 10 using a for loop
+
+// for(let i = 1 ; i<=10; i++){
+//     console.log(i)
+// }
+
+//print numbers from 10 to 1 using a while loop.
+
+// let i = 10;
+// while(i>=1){
+//     console.log(i)
+//     i--
+// }
+
+// print even numbers from 1 to 20 using a for loop
+
+// for (let i = 1; i <= 20; i++) {
+//   if (i % 2 === 0) console.log(i);
+// }
+
+// print even numbers from 1 to 15 using a while loop
+
+// let i = 1;
+// while (i <= 15) {
+//   if (i % 2 !== 0) {
+//     console.log(i);
+//   }
+//   i++;
+// }
+
+// print the multiplication table of 5
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`5 * ${i} = ${i * 5}`);
+// }
+
+// let i = 1;
+// while (i <= 10) {
+//   console.log(`5 * ${i} = ${i * 5}`);
+//   i++;
+// }
+
+// Find the sum of number from 1 to 100 using a loop
+// let sum = 0;
+// for (let i = 1; i <= 100; i++) {
+//   sum += i;
+// }
+// console.log(sum);
+
+// print all numbers between 1 to 50 that are divisible by 3
+
+// for (let i = 1; i <= 50; i++) {
+//   if (i % 3 === 0) {
+//     console.log(i);
+//   }
+// }
+
+// Ask the user for a number and print whether each number from 1 to 100
+//(e.g - 1 is odd , 2 is even)
+
+// let num = prompt("enter a number");
+// num = Number(num);
+
+// for (let i = 1; i <= num; i++) {
+//   if (i % 2 !== 0) {
+//     console.log(`${i} odd`);
+//   } else {
+//     console.log(`${i} even`);
+//   }
+// }
+
+//count how many numbers between 1 to 100 are divisible by both 3 and 5
+
+let counter = 0;
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    counter++;
+    console.log(i);
+  }
+}
+console.log(counter);
---
+++ javascript - Learn Everything part 1/controlflow/script.js
@@ -0,0 +1,40 @@
+// if else elseif
+// switch case
+// early return pattern
+
+// function getVal(val) {
+//   if (val < 25) return "D";
+//   else if (val < 50) return "C";
+//   else if (val < 75) return "C";
+//   else return "A";
+// }
+
+// console.log(getVal(100));
+
+//CONFUSION TOPICS
+
+// function getGrade(score) {
+//   if (score >= 90 && score <= 100) return "A+";
+//   else if (score >= 80 && score <= 89) return "A";
+//   else if (score >= 70 && score <= 79) return "B";
+//   else if (score >= 60 && score <= 69) return "C";
+//   else if (score >= 33 && score <= 59) return "D";
+//   else if (score >= 0 && score <= 32) return "fail";
+//   else return "Invalid Marks";
+// }
+// console.log(getGrade(10));
+
+// -----------------------------------
+
+// Rock paper scissor logic
+
+// function rps(user, computer) {
+//   if (user === computer) return "draw";
+
+//   if (user === "rock" && computer === "scissor") return "User wins";
+//   if (user === "scissor" && computer === "paper") return "User wins";
+//   if (user === "paper" && computer === "rock") return "User wins";
+
+//   return "Computer Wins";
+// }
+// console.log(rps("rock", "scissor"));
