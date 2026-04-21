ccf61bd026dd8abc7a87a76ffac0777c5a1dc3db
paawan-web-ai <matrixescape345@gmail.com>
2026-04-21 21:58:07 +0530

    function_CLOSURES

--- functions_pra/script.js
+++ functions_pra/script.js
@@ -95,3 +95,116 @@
 //     val4();
 //   });
 // });
+
+//what is a higher-order function?
+
+//A function which returns another function / accepts a funcition inside its parameter
+
+// function abcd() {
+//   return function () {
+//     console.log("hello");
+//   };
+// }
+
+// abcd()();
+
+// function abcd(fn1) {
+//   fn1();
+// }
+// abcd(function () {
+//   console.log("hello");
+// });
+
+// convert the above function into a pure function
+
+// let total = 0;
+// function addToTotal(num) {
+//   total += num;
+// }
+
+// let total = 0;
+
+// function addToTotal(num) {
+//   let newTotal = total;
+//   newTotal += num;
+//   console.log(`New Total: ${newTotal}`);
+// }
+// console.log(`Total: ${total}`);
+// addToTotal(5);
+
+//what is a closure? when is it created?
+// A function which returns another function is called closures and inside function is the value of outer function
+
+// function abcd() {
+//   let val = 0;
+//   return function () {
+//     console.log(val);
+//   };
+// }
+// abcd()();
+
+// ------------------------------------
+
+// function outer() {
+//   let count = 0;
+//   return function () {
+//     count++;
+//     console.log(count);
+//   };
+// }
+
+// let counter = outer();
+// counter();
+// counter();
+
+//-------------------
+
+//convert this normal function into an IIFE
+
+// function init() {
+//   console.log("initialized");
+// }
+
+// (() => {
+//   console.log("hello");
+// })();
+
+// (function () {
+//   console.log("initialized");
+// })();
+
+//use case of IIFE = when you want to create private variables
+
+// let shery = (function () {
+//   let score = 0;
+//   return {
+//     getScore: function () {
+//       console.log(score);
+//     },
+//     setScore: function (val) {
+//       score = val;
+//     },
+//   };
+// })();
+
+//BMI
+
+// function bmi(weight, height) {
+//   return weight / (height * height);
+// }
+
+// console.log(bmi(90, 1.7).toFixed(2));
+
+// ---------------------
+// discountcalculator
+
+function discountCalculator(discount) {
+  return function (price) {
+    return price - price * (discount / 100);
+  };
+}
+
+let tenPercentOff = discountCalculator(10);
+let twentyPercentOff = discountCalculator(20);
+
+console.log(tenPercentOff(100));
