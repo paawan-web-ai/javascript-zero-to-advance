d60a85d33c703246eafc6e3b56e3f6642fae0529
paawan-web-ai <matrixescape345@gmail.com>
2026-05-27 08:02:06 +0530

    Objects

---
+++ javascript - Learn Everything part 1/objects/script.js
@@ -0,0 +1,181 @@
+// let obj = {
+//   name: "harsh",
+//   age: 26,
+//   khaana: "daal chaawal",
+// };
+
+// console.log(obj.age); // correct
+// console.log(obj["name"]);// correct
+
+// --------------------------------------------
+
+// let obj = {
+//   name: "harsh",
+//   age: 26,
+//   khaana: "daal chaawal",
+// };
+
+// // let aa = "name";
+// // console.log(aa); no this 'aa' wont convert into name:"harsh"
+
+// //to convert it write like this
+
+// let aa = "name";
+// console.log(obj[aa]);
+
+// --------------------------------------------
+
+//nesting and deep access
+
+// let user = {
+//   name: "Harsh",
+//   address: {
+//     city: "bhopal",
+//     pin: 462001,
+//     location: {
+//       lat: 23.2,
+//       lng: 77.4,
+//     },
+//   },
+// };
+
+// console.log(user.address.location.lng);
+
+//Object De-structing
+// let { lat, lng } = user.address.location;
+// console.log(lat, lng);
+
+//-------------------------------------------
+//looping [for-in,objects.keys,object.entries]
+
+//for-in
+// let obj = {
+//   name: "harsh",
+//   age: 26,
+//   email: "test@test.com",
+// };
+// for (let key in obj) {
+//   //   console.log(key);
+//   console.log(key, obj[key]);
+// }
+
+/////////////
+
+//objects.keys
+
+// let obj = {
+//   name: "harsh",
+//   age: 26,
+//   email: "test@test.com",
+// };
+// console.log(Object.keys(obj));
+
+/////////////////
+
+// object.entries
+
+// let obj = {
+//   name: "harsh",
+//   age: 26,
+//   email: "test@test.com",
+// };
+
+// console.log(Object.entries(obj));
+
+//------------------------------------
+
+//Oject-assign , deep clone
+
+// let obj = {
+//   name: "harsh",
+//   age: 26,
+//   email: "test@test.com",
+// };
+//data will go inside obj2 (spread operator)
+// let obj2 = { ...obj };
+
+// console.log(obj2);
+
+//////////////////////////
+
+//object.assign - use to copy object
+
+// let obj = {
+//   name: "harsh",
+//   age: 26,
+//   email: "test@test.com",
+// };
+
+// let obj2 = Object.assign({}, obj);
+// console.log(obj2);
+
+// let obj = {
+//   name: "harsh",
+//   age: 26,
+//   email: "test@test.com",
+// };
+
+// let obj2 = Object.assign({ price: Infinity }, obj);
+
+// console.log(obj2);
+
+//deep cloning
+
+// let user = {
+//   name: "Harsh",
+//   address: {
+//     city: "bhopal",
+//     pin: 462001,
+//     location: {
+//       lat: 23.2,
+//       lng: 77.4,
+//     },
+//   },
+// };
+
+//convert object to string
+// JSON.stringify(user);
+
+//convert back to object
+// JSON.parse(user);
+
+// let obj2 = JSON.parse(JSON.stringify(user));
+
+// --------------------------------------
+
+// optional chaining and computed properties
+
+// let obj = {
+//   name: "Harsh",
+//   addresses: {
+//     city: "bhopal",
+//     pin: 462001,
+//     location: {
+//       lat: 23.2,
+//       lng: 77.4,
+//     },
+//   },
+// };
+// // if its there then return but dont give error
+// console.log(obj?.address?.city);
+
+//--------------------------------------
+
+//computed properties
+
+let role = "admin";
+
+let obj = {
+  name: "Harsh",
+  addresses: {
+    city: "bhopal",
+    pin: 462001,
+    location: {
+      lat: 23.2,
+      lng: 77.4,
+    },
+  },
+  [role]: "harsh",
+};
+
+console.log(obj);
