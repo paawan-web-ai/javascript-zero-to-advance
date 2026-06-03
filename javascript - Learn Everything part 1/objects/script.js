e4305a9f49a5c84c40be6fd6a1fa492372c8e1b1
paawan-web-ai <matrixescape345@gmail.com>
2026-06-03 23:37:05 +0530

    js_obj

--- javascript - Learn Everything part 1/objects/script.js
+++ javascript - Learn Everything part 1/objects/script.js
@@ -41,9 +41,11 @@
 
 // console.log(user.address.location.lng);
 
-//Object De-structing
+// Object De-structing
 // let { lat, lng } = user.address.location;
+// let { city, pin } = user.address;
 // console.log(lat, lng);
+// console.log(city, pin);
 
 //-------------------------------------------
 //looping [for-in,objects.keys,object.entries]
@@ -163,19 +165,80 @@
 
 //computed properties
 
-let role = "admin";
+// let role = "admin";
 
-let obj = {
-  name: "Harsh",
-  addresses: {
-    city: "bhopal",
-    pin: 462001,
-    location: {
-      lat: 23.2,
-      lng: 77.4,
-    },
+// let obj = {
+//   name: "Harsh",
+//   addresses: {
+//     city: "bhopal",
+//     pin: 462001,
+//     location: {
+//       lat: 23.2,
+//       lng: 77.4,
+//     },
+//   },
+//   [role]: "harsh",
+// };
+
+// console.log(obj);
+
+// Create an object for a student with name ,age, and  isEnrolled
+
+// let student = {
+//   name: "Harsh",
+//   age: 26,
+//   isEnrolled: true,
+// };
+
+// can an  object key be a number or boolean ? try this
+
+// const obj = {
+//   true: "yes",
+//   42: "answer",
+// };
+
+// console.log(obj[42]);
+// let { 42: value } = obj;
+// console.log(value);
+
+// Access the value of "first - name" from this object :
+// const user = {
+//   "first-name": "harsh",
+// };
+
+// console.log(user["first-name"]);
+
+//Given a  dynamic key let key = "age", how will you access user[key]
+
+// let key = "age";
+// const user = {
+//   key: "abcd",
+//   age: 26,
+// };
+// === key -> age->26 ===
+// console.log(user[key]);
+
+//from the object below print the latitude
+
+// const location = {
+//   city: "bhopal",
+//   cordinates: {
+//     lat: 23.2,
+//     lng: 77.4,
+//   },
+// };
+
+// let { lat } = location.cordinates;
+// console.log(lat);
+
+// what will happen if coordinates is missing ? how can you prevent errors?
+
+const location = {
+  city: "bhopal",
+  cordinates: {
+    lat: 23.2,
+    lng: 77.4,
   },
-  [role]: "harsh",
 };
 
-console.log(obj);
+console.log(location?.cordinates?.Lat);
