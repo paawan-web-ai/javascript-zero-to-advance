c854efd3a291942606a80c34f7703fec9b89c8d8
paawan-web-ai <matrixescape345@gmail.com>
2026-06-13 08:18:22 +0530

    input_dom

--- Part 2 – Master JavaScript & Become a Real Developer/THE_DOM/script.js
+++ Part 2 – Master JavaScript & Become a Real Developer/THE_DOM/script.js
@@ -1,9 +1,50 @@
-let h1 = document.querySelector(".header");
+// let h1 = document.querySelector(".header");
 
-// console.log(h1.inner);
+// // console.log(h1.inner);
 
-// understand this
+// // understand this
 
-console.dir(h1);
+// console.dir(h1);
 
-h1.innerText = " hello ram kaise ho";
+// h1.innerText = " hello ram kaise ho";
+
+// let header = document.querySelector(".header");
+
+// header.addEventListener("dblclick", () => {
+//   header.style.color = "red";
+// });
+
+//remove event listner
+// let header = document.querySelector(".header");
+
+// let sum = 0;
+// function dblclick() {
+//   if (sum === 0) {
+//     header.style.color = "red";
+//     sum++;
+//   } else {
+//     header.style.color = "black";
+//     sum--;
+//   }
+// }
+
+// header.addEventListener("dblclick", dblclick);
+// header.removeEventListener("dblclick", dblclick);
+
+// input
+
+// let input = document.querySelector("input");
+
+// input.addEventListener("input", () => {
+//   console.log(input.value);
+// });
+
+// back space
+
+let input = document.querySelector("input");
+
+input.addEventListener("input", (dets) => {
+  if (dets.data !== null) {
+    console.log(dets.data);
+  }
+});
