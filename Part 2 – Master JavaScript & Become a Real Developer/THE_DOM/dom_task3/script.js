cb4d5dab0139323f41242d22506672d6dc9f995a
paawan-web-ai <matrixescape345@gmail.com>
2026-06-30 22:03:29 +0530

    create_card_task3

--- Part 2 – Master JavaScript & Become a Real Developer/THE_DOM/dom_task3/script.js
+++ Part 2 – Master JavaScript & Become a Real Developer/THE_DOM/dom_task3/script.js
@@ -1,6 +1,36 @@
 let form = document.querySelector("form");
-
+let inputs = document.querySelectorAll("input");
+let cardsContainer = document.querySelector("#cards-container");
 form.addEventListener("submit", (dets) => {
-  console.log(dets);
+  // console.log(dets);
   dets.preventDefault();
+
+  let card = document.createElement("div");
+  card.classList.add("card");
+
+  let profile = document.createElement("div");
+  profile.classList.add("profile");
+
+  let img = document.createElement("img");
+  img.setAttribute(
+    "src",
+    "https://plus.unsplash.com/premium_photo-1779753391100-6ed78213c5b4?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2fHx8ZW58MHx8fHx8",
+  );
+
+  let h3 = document.createElement("h3");
+  h3.textContent = "rahul";
+
+  let h5 = document.createElement("h5");
+  h5.textContent = "stars";
+
+  let p = document.createElement("p");
+  p.textContent = "all around him";
+
+  profile.appendChild(img);
+  card.appendChild(profile);
+  card.appendChild(h3);
+  card.appendChild(h5);
+  card.appendChild(p);
+
+  cardsContainer.appendChild(card);
 });
